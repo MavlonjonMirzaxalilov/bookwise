bfa09af2ccfad9bf1629a0c6b53fd04b814d6e1b
MavlonjonMirzaxalilov <mirzaxalilovmavlonjon7@gmail.com>
2025-01-17 09:00:51 +0500

    imagekit added

--- components/ImageUpload.tsx
+++ components/ImageUpload.tsx
@@ -1,9 +1,18 @@
 "use client";
 import { IKImage, ImageKitProvider, IKUpload } from "imagekitio-next";
+import config from "@/lib/config";
 
 const authentator = async () => {
   try {
-    const response = await fetch();
+    const response = await fetch(`${config.env.apiEndpoint}/api/auth/imagekit`);
+    if (!response.ok) {
+      const errorText = await response.text();
+
+      throw new Error(
+        `Request failed with status ${response.status}: ${errorText}`,
+      );
+    }
+    const data = await response.json();
   } catch (error: any) {
     throw new Error(`Authentication request failed:${error.messages}`);
   }
